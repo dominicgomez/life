fd8b59e7f903c758d8ac32acb1efe1d891e5fb8d
Dominic Gomez <dominicgomez@users.noreply.github.com>
2022-07-13 19:55:52 -0700

    choo choo CHOO

--- src/life.c
+++ src/life.c
@@ -4,6 +4,18 @@
 #include "conf.h"
 #include "util.h"
 
+Pattern gen_seed(Size size, double density)
+{
+    size_t cells = size.x * size.y;
+    size_t live_cells = (int)(cells * density);
+    Pattern seed;
+    seed.live_cells = malloc(sizeof(Cell) * live_cells);
+    seed.size = 0;
+    seed.cap = live_cells;
+
+    return seed;
+}
+
 int main()
 {
     srand(time(NULL));
@@ -26,10 +38,10 @@ int main()
             break;
         }
 
-        size_t coords = (size_t)(LINES * COLS) * DENSITY;
+        Size term_size = { LINES, COLS };
+        Pattern seed = gen_seed(term_size, DENSITY);
 
-        for (size_t i = 0; i < coords; ++i) {
-        }
+        free(seed.live_cells);
     }
 
     resetty();
--- src/util.h
+++ src/util.h
@@ -1,6 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-typedef struct Pair { int y, x; } Pair;
+#include <stddef.h>
+
+typedef struct Pair { int y, x; } Size, Cell;
+
+typedef struct {
+    size_t size, cap;
+    Cell *live_cells;
+} Pattern;
 
 #endif
